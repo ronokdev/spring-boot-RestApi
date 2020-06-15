a7b06341e218a5d192117e5ded0d797037608406
Ronok <fuad.ronok@gmail.com>
2020-06-15 22:31:54 +0600

    - Add context path to the application

--- src/main/java/com/ronok/springweb/restapi/RestapiApplication.java
+++ src/main/java/com/ronok/springweb/restapi/RestapiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//This will exclue the auto configuration of the 'DataSourceAutoConfiguration.class' on application startup
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class RestapiApplication {
 
 	public static void main(String[] args) {
