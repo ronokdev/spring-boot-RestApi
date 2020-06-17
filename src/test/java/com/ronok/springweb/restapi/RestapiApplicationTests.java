c4dab35caa2f70064801c4f50bf0a2c3cc979f06
Ronok <fuad.ronok@gmail.com>
2020-06-17 15:42:51 +0600

    test(get product): unit test writing done for Getting a product

--- src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
+++ src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ronok.springweb.restapi;
 
+import com.ronok.springweb.restapi.entities.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class RestapiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testGetProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/springbootapi/products/3",Product.class);
+		assertNotNull(product);
+		assertEquals("Xioami",product.getName());
+
 	}
 
 }
