c790696f05c4b7477640ab86e94a2af0ab0982bd
Ronok <fuad.ronok@gmail.com>
2020-06-17 18:13:23 +0600

    test(Create product): unit test writing done for Creating a product

--- src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
+++ src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
@@ -18,4 +18,20 @@ class RestapiApplicationTests {
 
 	}
 
+
+	@Test
+	public void testCreateProduct()
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = new Product();
+		product.setName("Samsung");
+		product.setDescription("This is Samsung Phone");
+		product.setPrice(70000);
+		Product newProduct = restTemplate.postForObject("http://localhost:8080/springbootapi/products/",product,Product.class);
+
+		assertNotNull(newProduct);
+		assertNotNull(newProduct.getId());
+		assertEquals("Samsung",newProduct.getName());
+
+	}
 }
