66dded084814bd7d02a8a26336df155be55d6e0b
Ronok <fuad.ronok@gmail.com>
2020-07-20 18:11:12 +0600

    feat(batch-step):  All the beans has been initialized inorder to create a STEP

--- src/main/java/com/ronok/springweb/restapi/batch/batchConfig.java
+++ src/main/java/com/ronok/springweb/restapi/batch/batchConfig.java
@@ -1,11 +1,28 @@
 package com.ronok.springweb.restapi.batch;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class batchConfig
 {
+
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step()
+    {
+        return stepBuilderFactory.get("step_1")
+                .<String,String>chunk(1)
+                .reader(reader())
+                .writer(writer())
+                .processor(processor())
+                .build();
+    }
+
+
     @Bean
     public Reader reader()
     {
