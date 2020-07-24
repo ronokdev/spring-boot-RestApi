aa1f69dcc58ecfcc55bc34e2b612561e7b7c6bf1
Ronok <fuad.ronok@gmail.com>
2020-07-24 21:56:06 +0600

    feat(applicatin-Batch): Test call has been written to test BATCH

--- src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
+++ src/test/java/com/ronok/springweb/restapi/RestapiApplicationTests.java
@@ -2,6 +2,15 @@ package com.ronok.springweb.restapi;
 
 import com.ronok.springweb.restapi.entities.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
@@ -50,6 +59,20 @@ class RestapiApplicationTests {
 
 		restTemplate.put(productapiurl,product);
 
+	}
+
+
+	// Test Batch
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
+	public void testBatch() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException
+	{
+		JobParameters jobParameters = new JobParametersBuilder().addString(name,"Ronok").toJobParameters();
+		jobLauncher.run(job,jobParameters);
 	}
 
 }
