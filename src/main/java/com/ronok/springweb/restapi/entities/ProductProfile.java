28bb0512730ffcf7aa3e1eed752d7827efea3f85
Ronok <fuad.ronok@gmail.com>
2020-09-11 15:08:53 +0600

    feature(application): one to one relation implimented between two Models

--- src/main/java/com/ronok/springweb/restapi/entities/ProductProfile.java
+++ src/main/java/com/ronok/springweb/restapi/entities/ProductProfile.java
@@ -3,6 +3,7 @@ package com.ronok.springweb.restapi.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -12,23 +13,16 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Entity
-@Table(name = "product")
-public class Product implements Serializable
+@Table(name = "product_profile")
+public class ProductProfile implements Serializable
 {
     public static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
-    @NotNull(message = "Name Can Not Be NULL")
-    public String name;
     @Size(max = 100)
-    public String description;
-    @Min(value = 1,message = "Price Must Be Grater than 1")
-    public int price;
+    public String description_details;
 
 
-    @OneToOne
-    @JoinColumn(name = "product_profile_id")
-    private ProductProfile productProfile;
 }
